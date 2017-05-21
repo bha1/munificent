bccc6e5b08b39cd13839b16b117553c73eb84a27
bha1 <peeyushbhawan@gmail.com>
2017-05-21 23:05:32 +1000

    repo init

---
+++ src/main/java/com/munificent/rest/HeyMateWebService.java
@@ -0,0 +1,18 @@
+package com.munificent.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+@Path("/hello")
+public class HeyMateWebService {
+
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg){
+		String output = "Jersey says :"+msg;
+		
+		return Response.status(200).entity(output).build();
+	}
+}
